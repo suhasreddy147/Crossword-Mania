4cb1a436906bd61c0625851832d2603f73af45e2
suhasreddy147 <suhasreddy147@gmail.com>
2026-02-06 23:43:31 +0530

    CM-21 Add settings and store entry points

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -3,8 +3,9 @@ extends Control
 var has_active_game: bool = false
 
 @onready var continue_button: Button = $MainLayoutMarginContainer/MainLayout/ButtonsContainer/ContinueButton
-
 @onready var continue_info_label: Label = $MainLayoutMarginContainer/MainLayout/ButtonsContainer/ContinueInfoLabel
+@onready var settings_button: TextureButton = $MainLayoutMarginContainer/MainLayout/TopBarContainer/SettingsContainer/SettingsButton
+@onready var store_button: TextureButton = $MainLayoutMarginContainer/MainLayout/TopBarContainer/StoreContainer/StoreButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,3 +18,9 @@ func update_continue_button_state() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func _on_settings_button_pressed() -> void:
+	print("Settings entry point pressed (placeholder)") # Replace with function body.
+
+func _on_store_button_pressed() -> void:
+	print("Store entry point pressed (placeholder)") # Replace with function body.
